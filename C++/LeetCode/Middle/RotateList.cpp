773ce6cdcc3fbb7c3dcec496d6f2aa5669faf8d4
VitalianoParmezano <163889654+VitalianoParmezano@users.noreply.github.com>
2025-05-30 11:45:22 +0300

    Update RotateList.cpp
    100%

--- C++/LeetCode/Middle/RotateList.cpp
+++ C++/LeetCode/Middle/RotateList.cpp
@@ -1,33 +1,30 @@
-#include <iostream>
-
- struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
-  
-    ListNode* rotateRight(ListNode* head, int k);
-    void printList(ListNode* head);
-    ListNode*rotateRightRecurce(ListNode* head, int k);
-
-int main()
-{
-    ListNode* head = new ListNode(0);
-    ListNode* cursor = head;
-    for(int i = 1;i<10;i++){
-        cursor->next = new ListNode(i);
-        cursor=cursor->next;
-    }
-    printList(head);
-    head = rotateRightRecurce(head, 9);
-    //head = rotateRight (head, 2);
-    printList(head);
-    return 0;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+ListNode* rotateRight(ListNode* head, int k) {
+        if (!head){return nullptr;}
+        if (k <= 0||head->next==nullptr){return head;}
+        ListNode* cursor = head;
+        int counter;
+        while(cursor->next){
+            cursor=cursor->next;
+            counter++;
+        }
+        k=k%counter;
+        
+    return rotateRightWithNormalInput(head,k);
 }
-
-ListNode*rotateRightRecurce(ListNode* head, int k){
+private:
+    ListNode* rotateRightWithNormalInput(ListNode* head, int k){
     if (k <= 0||head->next==nullptr){return head;}
     ListNode* cursor = new ListNode(-1);
     cursor->next = head;
@@ -39,44 +36,6 @@ ListNode*rotateRightRecurce(ListNode* head, int k){
     ListNode*temp = cursor->next;
     cursor->next = nullptr;
     temp->next=head;
-    return rotateRightRecurce(temp,k-1);
-
-}
-
-ListNode* rotateRight(ListNode* head, int k) {
-        ListNode* cursor = head;
-        int counter = 0;
-
-        while (cursor->next){
-            counter++;
-            cursor = cursor->next;
-        }
-        counter -= k;
-
-        cursor = head;
-        for(int i = 0; i<counter; i++){
-            cursor = cursor->next;
-        }
-
-        ListNode* tail = cursor->next;
-        ListNode* NewHead = cursor->next;
-        cursor->next=nullptr; 
-
-        while(tail->next){
-            tail=tail->next;
-        } 
-        tail->next=head;
-
-        return NewHead;
-}
-       
-
-void printList(ListNode* head){
-    ListNode* cursor = head;
-    
-    while(cursor){
-        std::cout<<"\t"<<cursor->val<<std::endl;
-        cursor=cursor->next;
+    return rotateRightWithNormalInput(temp,k-1);
     }
-    std::cout<<std::endl;
-}
+};
